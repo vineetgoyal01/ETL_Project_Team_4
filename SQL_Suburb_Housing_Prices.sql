792970241c77189d15a8f876a4322557440f7143
Vineet Goyal <vineetgoyal01@gmail.com>
2020-08-18 22:17:52 +1000

    final data

---
+++ SQL_Suburb_Housing_Prices.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Melbourne_Housing_Data_Suburb (
+	
+	
+	"Suburb Name" VARCHAR(100) NOT null,
+	Adult_Population VARCHAR(200),
+	"Local Government Area" VARCHAR(200),
+	Postcode int,
+	"2016 Median Sold Price" int
+	
+	);
+	
+Select "Suburb Name", "2016 Median Sold Price" from Melbourne_Housing_Data_Suburb;
